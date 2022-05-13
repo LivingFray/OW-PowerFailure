b94229ed3eab38c69e8759c086aed97318b88add
BuildTools <unconfigured@null.spigotmc.org>
2022-05-13 20:39:39 +0100

    Renamed template

---
+++ ModTemplate/PowerFailure.cs
@@ -0,0 +1,20 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace PowerFailure
+{
+    public class PowerFailure : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            
+        }
+    }
+}
