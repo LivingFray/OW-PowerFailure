5b8cc227db733b1cdd7458913134cb159aeb46e9
BuildTools <unconfigured@null.spigotmc.org>
2022-05-14 01:28:46 +0100

    Implemented powering down of: screens, bulb emissive texture, bulb light source

--- PowerFailure/PowerFailure.cs
+++ PowerFailure/PowerFailure.cs
@@ -1,20 +1,75 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
+using UnityEngine;
 
 namespace PowerFailure
 {
     public class PowerFailure : ModBehaviour
     {
-        private void Awake()
-        {
-            // You won't be able to access OWML's mod helper in Awake.
-            // So you probably don't want to do anything here.
-            // Use Start() instead.
-        }
+        private RingWorldScreenController _screenController;
+
+        private RingWorldController _ringWorldController;
+
+        private OWEmissiveRenderer _ringWorldBulb;
+
+        private Light _ringWorldLight;
+
+        private float _originalIntensity;
+
+        private Color _originalBulbColour;
+
+        private bool _initialised;
 
         private void Start()
         {
-            
+            _initialised = false;
+            LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
         }
+
+        private void Update()
+        {
+            if (!_initialised) return;
+
+            if (TimeLoop.GetSecondsElapsed() < _ringWorldController.damBreakTime) return;
+
+            float PowerPercentage = Mathf.Max(0.0f, 1.0f - (TimeLoop.GetSecondsElapsed() - _ringWorldController.damBreakTime) / (_ringWorldController._lighthouseCollapseTime - _ringWorldController.damBreakTime));
+
+            for (int i = 0; i < (int)_screenController._screenRenderers.Length; i++)
+            {
+                _screenController._screenRenderers[i].SetMaterialProperty(RingWorldScreenController.s_propID_screenFlicker, PowerPercentage);
+            }
+
+            // Rescale the power value to account for light falloff looking awful when done linearly
+            float LightLevel = Mathf.Pow(PowerPercentage * _originalIntensity, 1.0f / 2.7f);
+
+            _ringWorldLight.intensity = LightLevel;
+            _ringWorldBulb.SetEmissionColor(_originalBulbColour * LightLevel);
+        }
+
+        private void OnCompleteSceneLoad(OWScene oldScene, OWScene newScene)
+        {
+            if (newScene == OWScene.SolarSystem)
+            {
+                _screenController = FindObjectOfType<RingWorldScreenController>();
+                _ringWorldController = FindObjectOfType<RingWorldController>();       
+                _ringWorldLight = GameObject.Find("RingWorld_Body/Sector_RingInterior/Lights_RingInterior/IP_SunLight").GetComponent<Light>();
+                _ringWorldBulb = GameObject.Find("RingWorld_Body/Sector_RingInterior/Geometry_RingInterior/Structure_IP_ArtificialSun/ArtificialSun_Bulb").GetComponent<OWEmissiveRenderer>();
+
+                _originalIntensity = _ringWorldLight.intensity;
+                _originalBulbColour = _ringWorldBulb.GetOriginalEmissionColor();
+
+                _initialised = true;
+            }
+            else
+            {
+                _screenController = null;
+                _ringWorldController = null;
+                _ringWorldLight = null;
+                _ringWorldBulb = null;
+
+                _initialised = false;
+            }
+        }
+
     }
 }
